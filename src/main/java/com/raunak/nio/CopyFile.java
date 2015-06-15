0beb96e1d5bbf0d7a34acade9056e2b6b897c8d4
raunak.agrawal <raunak.agrawal@inmobi.com>
2015-06-15 12:39:42 +0530

    Added test cases and made corrections in the code.

---
+++ src/main/java/com/raunak/nio/CopyFile.java
@@ -0,0 +1,37 @@
+package com.raunak.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class CopyFile {
+    static public void main(String args[]) throws Exception {
+
+
+        String infile = "/Users/raunak.agrawal/Test1.txt";
+        String outfile = "/Users/raunak.agrawal/Test2.txt";
+
+        FileInputStream fin = new FileInputStream(infile);
+        FileOutputStream fout = new FileOutputStream(outfile);
+
+        FileChannel fcin = fin.getChannel();
+        FileChannel fcout = fout.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+
+        while (true) {
+            buffer.clear();
+
+            int r = fcin.read(buffer);
+
+            if (r == -1) {
+                break;
+            }
+
+            buffer.flip();
+
+            fcout.write(buffer);
+        }
+    }
+}
