e3170a970a18fcf952363e92cd97a7f5190a1457
raunak.agrawal <raunak.agrawal@inmobi.com>
2015-08-03 18:10:00 +0530

    Merge branch 'develop' of github.corp.inmobi.com:inm-platform/rbac into phase2.1
    Conflicts:
    	authorization-client/pom.xml
    	authorization-client/src/main/java/com/inmobi/platform/rbac/authorization/thrift/client/AuthorizationClient.java
    	authorization-idl/pom.xml
    	authorization-service/conf-templates/logback.xml
    	authorization-service/pom.xml
    	authorization-service/src/main/java/com/inmobi/platform/rbac/auth/service/AuthorizationService.java
    	authorization-service/src/main/java/com/inmobi/platform/rbac/auth/service/cache/UserRoleCache.java
    	authorization-service/src/main/java/com/inmobi/platform/rbac/auth/service/impl/AuthorizationServiceImpl.java
    	authorization-service/src/main/java/com/inmobi/platform/rbac/auth/service/modules/AuthModule.java
    	authorization-service/src/test/java/com/inmobi/platform/rbac/auth/service/cache/UserRoleCacheTest.java
    	authorization-service/src/test/java/com/inmobi/platform/rbac/auth/service/impl/AuthorizationServiceImplTest.java
    	pom.xml
    	provisioning-client/pom.xml
    	provisioning-idl/pom.xml
    	provisioning-service/pom.xml
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/converter/PrivilegeConverter.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/converter/RoleConverter.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/converter/UserRoleConverter.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/handler/RbacProvisioningServiceHandler.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/indis/ProvisionCliService.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/modules/ProvisioningModule.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/search/role/RoleCriterias.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/service/impl/ProfileServiceImpl.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/service/impl/UserRoleServiceImpl.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/validator/ProfileServiceValidator.java
    	provisioning-service/src/main/java/com/inmobi/platform/rbac/provisioning/validator/ProfileServiceValidatorImpl.java
    	provisioning-service/src/test/java/com/inmobi/platform/rbac/provisioning/service/impl/AllMocks.java
    	provisioning-service/src/test/java/com/inmobi/platform/rbac/provisioning/validator/UserRoleValidatorTest.java
    	provisioning-service/src/test/java/com/inmobi/platform/rbac/provisioning/validator/ValidatorTest.java
    	rbac-commons/pom.xml
    	rbac-commons/src/main/java/com/inmobi/platform/rbac/commons/dao/UserFilterDao.java
    	rbac-commons/src/main/java/com/inmobi/platform/rbac/commons/dao/impl/UserFilterDaoImpl.java
    	rbac-helper/pom.xml

--- src/main/java/com/java/eight/tutorial/lambda/FilteringApples.java
+++ src/main/java/com/java/eight/tutorial/lambda/FilteringApples.java
@@ -1,17 +1,18 @@
 package com.java.eight.tutorial.lambda;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class FilteringApples {
 
     public static void main(String... args) {
 
-        List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"));
+        List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"),
+                new Apple(120, "black"));
 
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
@@ -21,8 +22,9 @@ public class FilteringApples {
         List<Apple> heavyApples = filterApples(inventory, FilteringApples::isHeavyApple);
         System.out.println(heavyApples);
 
-        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
-        List<Apple> greenApples2 = filterApples(inventory, a -> "green".equals(a.getColor()));
+        System.out.println("Not green apples");
+        Predicate<Apple> greenP = a -> "green".equals(a.getColor());
+        List<Apple> greenApples2 = filterApples(inventory, greenP.negate().and(a -> a.getWeight() == 120));
         System.out.println(greenApples2);
 
         // [Apple{color='green', weight=155}]
@@ -34,7 +36,9 @@ public class FilteringApples {
                 (Apple a) -> a.getWeight() < 80 || "brown".equals(a.getColor()));
         System.out.println(weirdApples);
 
-        //List<Apple> s = inventory.stream().filter(a -> a.getWeight() > 150).collect();
+        // Sorting apples
+        System.out.println("Sorting Apples");
+        FilteringApples.printSortedApples(inventory);
     }
 
     public static boolean isGreenApple(Apple apple) {
@@ -55,6 +59,14 @@ public class FilteringApples {
         return result;
     }
 
+    public static void printSortedApples(List<Apple> apples) {
+
+        apples.sort(Comparator.comparing(Apple::getWeight).reversed().thenComparing(Apple::getColor).reversed());
+
+        apples.forEach(System.out::println);
+
+    }
+
     public static class Apple {
         private final int    weight;
         private final String color;
@@ -73,6 +85,7 @@ public class FilteringApples {
         }
 
         public String toString() {
+            BiFunction<Integer, String, Apple> getApp = Apple::new;
             return "Apple{" + "color='" + color + '\'' + ", weight=" + weight + '}';
         }
     }
