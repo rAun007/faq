4255f33f513738d16d0f8633603902ae878b725b
raunak.agrawal <raunak.agrawal@inmobi.com>
2015-08-03 18:52:08 +0530

    Capturing Lambda.

---
+++ src/main/java/com/java/eight/tutorial/lambda/CapturingLambda.java
@@ -0,0 +1,21 @@
+package com.java.eight.tutorial.lambda;
+
+/**
+ * Created by raunak.agrawal on 8/1/15.
+ */
+public class CapturingLambda {
+
+    public static void main(String[] args) {
+
+        /**
+         * Lambdas are allowed to capture (that is, to reference in their bodies) instance variables and static
+         * variables without restrictions. But local variables have to be explicitly declared final or are effectively
+         * final. In other words, lambda expressions can capture local variables that are assigned to them only once.
+         * (Note: capturing an instance variable can be seen as capturing the final local variable this.)
+         */
+        int pNo = 8080; // The
+        Runnable r = () -> System.out.println(pNo);
+        //pNo = 6; //Compilation error
+        System.out.println("Done");
+    }
+}
