b8c32a0554c3ec9cb7f1d2e9e3e8080efe4a9f26
raunak.agrawal <raunak.agrawal@inmobi.com>
2016-10-17 14:29:01 +0530

    Added the Kadane's algo.

--- src/main/java/com/raunak/algo/MaxSubArray.java
+++ src/main/java/com/raunak/algo/MaxSubArray.java
@@ -6,12 +6,41 @@ public class MaxSubArray {
 
     public static void main(String[] args) {
 
-        int[] a = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+        int[] a = { 13, -3, -25, 20, -3, -16, -23, -41, 18, 20, -7, 12, -5, -22, 15, -4, 7};
 
         int low = 0;
         int high = a.length - 1;
 
         System.out.println(maxSub(a, low, high));
+
+        System.out.println(kadaneAlgo(a));
+    }
+
+    private static int kadaneAlgo(int[] a) {
+
+        /**
+         * Simple idea of the Kadane's algorithm is to look for all positive contiguous segments of the array
+         * (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive
+         * segments (max_so_far is used for this). Each time we get a positive sum compare it with max_so_far and update
+         * max_so_far if it is greater than max_so_far
+         */
+        int max_ending_here = 0;
+        int max_so_far = 0;
+
+        for (int i = 0; i < a.length; i++) {
+
+            max_ending_here = max_ending_here + a[i];
+
+            if (max_ending_here < 0) {
+                max_ending_here = 0;
+            }
+
+            if (max_ending_here > max_so_far) {
+                max_so_far = max_ending_here;
+            }
+        }
+
+        return max_so_far;
     }
 
     private static Triplet<Integer, Integer, Integer> maxSub(int[] a, int low, int high) {
